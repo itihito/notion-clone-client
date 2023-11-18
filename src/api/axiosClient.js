938cfd2ed4b5e52d066faf10f8f2cf37134b6a1c
itihito <itihito>
2023-11-18 15:41:17 +0900

    APIURL修正

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+// const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://notion-clone-server.onrender.com/api/v1";
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
