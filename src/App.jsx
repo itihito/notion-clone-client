448ad2afee5fed8f35e8c8fd0a31236b54cbf1fc
itihito <itihito>
2023-11-18 18:47:25 +0900

    画面更新で404になる件の修正3

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,17 @@ import { blue } from "@mui/material/colors";
 import AppLayout from "./components/layout/AppLayout";
 import Home from "./pages/Home";
 import Memo from "./pages/Memo";
+import { useEffect } from "react";
+
+const RedirectToIndex = () => {
+  // 404エラー時にリダイレクトする処理
+  useEffect(() => {
+    window.location.href = "/";
+  }, []);
+
+  // リダイレクトが行われた場合、実際にはこのコンポーネントが表示される前にページが切り替わる
+  return null;
+};
 
 function App() {
   const theme = createTheme({
@@ -25,7 +36,9 @@ function App() {
           <Route path="/" element={<AppLayout />}>
             <Route index element={<Home />} />
             <Route path="memo" element={<Home />} />
-            <Route path="memo/:memoId" element={<Memo />}></Route>
+            <Route path="memo/:memoId" element={<Memo />} />
+            {/* 404エラー時にリダイレクト */}
+            <Route path="*" element={<RedirectToIndex />} />
           </Route>
         </Routes>
       </BrowserRouter>
